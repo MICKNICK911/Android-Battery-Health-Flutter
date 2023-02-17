a3467e5705d627fe39e84dd0cddbae0756405184
MICHAEL APPIAH <105696656+MICKNICK911@users.noreply.github.com>
2023-02-17 16:03:41 +0000

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,160 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:battery_plus/battery_plus.dart';
+
+// void main() {
+// runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	title: 'Flutter Demo',
+	theme: ThemeData(
+		primarySwatch: Colors.blue,
+	),
+	home: const MyHomePage(),
+	);
+}
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+const MyHomePage({
+	Key? key,
+}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+@override
+State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+var battery = Battery();
+int percentage = 0;
+late Timer timer;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// created a Batterystate of enum type
+BatteryState batteryState = BatteryState.full;
+late StreamSubscription streamSubscription;
+																			
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+@override
+void initState() {
+	super.initState();
+	// calling the method to get battery state
+	getBatteryState();
+
+	// calling the method to get battery percentage
+	Timer.periodic(const Duration(seconds: 5), (timer) {
+	getBatteryPerentage();
+														
+	});
+	
+														
+}
+// method created to display battery percent
+void getBatteryPerentage() async {
+	final level = await battery.batteryLevel;
+	percentage = level;
+
+	setState(() {});
+}
+
+// method to know the state of the battery
+void getBatteryState() {
+	streamSubscription = battery.onBatteryStateChanged.listen((state) {
+	batteryState = state;
+
+	setState(() {});
+	});
+}
+
+// Custom widget to add different states of battery
+// ignore: non_constant_identifier_names
+Widget BatteryBuild(BatteryState state) {
+	switch (state) {
+
+	// first case is for battery full state
+	// then it will show green in color
+	case BatteryState.full:
+		// ignore: sized_box_for_whitespace
+		return Container(
+		width: 200,
+		height: 200,
+			
+		// ignore: prefer_const_constructors
+		child: (Icon(
+			Icons.battery_full,
+			size: 200,
+			color: Colors.green,
+		)),
+		);
+
+	// Second case is when battery is charging
+	// then it will show blue in color
+	case BatteryState.charging:
+		
+		// ignore: sized_box_for_whitespace
+		return Container(
+		width: 200,
+		height: 200,
+			
+		// ignore: prefer_const_constructors
+		child: (Icon(
+			Icons.battery_charging_full,
+			size: 200,
+			color: Colors.blue,
+		)),
+		);
+
+		// third case is when the battery is
+		// discharged then it will show red in color
+	case BatteryState.discharging:
+	default:
+		
+		// ignore: sized_box_for_whitespace
+		return Container(
+		width: 200,
+		height: 200,
+			
+		// ignore: prefer_const_constructors
+		child: (Icon(
+			Icons.battery_alert,
+			size: 200,
+			color: Colors.red,
+		)),
+		);
+	}
+}
+
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(
+		backgroundColor: Colors.green,
+		title: const Text('GeeksforGeeks',
+							style: TextStyle(color: Colors.white),),
+		centerTitle: true,
+	),
+	body: Center(
+		child: Column(
+			
+		mainAxisAlignment: MainAxisAlignment.center,
+		children:[
+			
+			// calling the custom widget
+			BatteryBuild(batteryState),
+			
+			// Displaying battery percentage
+			Text('Battery Percentage: $percentage',
+				style: const TextStyle(fontSize: 24),)
+			],
+		),
+	),
+	);
+}
 }
